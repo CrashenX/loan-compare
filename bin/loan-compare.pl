792565dcbf1b17b154b5ea9fb2ef71eca78c3ad2
Jesse J. Cook <code.crashenx@gmail.com>
2012-04-14 18:30:25 -0500

    initial future value calculations

--- bin/loan-compare.pl
+++ bin/loan-compare.pl
@@ -1,12 +1,21 @@
 #!/usr/bin/perl
 use warnings FATAL => 'all';
 use strict;
+use Getopt::Long;
 use lib "../lib/";
 use Finance::Amortization;
+
+my $options = GetOptions (
+    "t|tax"    => \(my $use_tax=0),
+    "h|help"   => \(my $help=0)
+);
 
 my $compounding = 12;
 my $years = 19;
 my $periods = $compounding * $years;
+my $earn_rate = 0.07 / $compounding;
+my $initial_investment = 0;
+my $allot = 3000;
 
 my $am1 = new Finance::Amortization(
     principal => 165000,
@@ -17,9 +26,27 @@ my $am1 = new Finance::Amortization(
     precision => 2
 );
 
-for(my $i = 0; $i < $period; ++$i) {
+my $schedule = $am1->schedule();
+my $payments = @$schedule;
+my $invest   = $initial_investment;
+for(my $period = 0; $period < $periods; ++$period) {
+    my $subtract = 0;
 
+    if($period < $payments) {
+        my $p = $schedule->[$period]->{'principal'};
+        my $i = $schedule->[$period]->{'interest'};
+        my $t = ($use_tax) ? $schedule->[$period]->{'tax_savings'} : 0;
+
+        $subtract += ($p + $i - $t);
+        die() if(0 > $subtract);
+    }
+
+    $invest *= ($earn_rate+1);
+    $invest += $allot;
+    $invest -= $subtract;
 }
+
+printf("Future Value: %.*f\n", $am1->{'precision'}, $invest);
 
 #my $principal = $amortization->principal();
 #my $rate = $amortization->rate();
@@ -39,4 +66,4 @@ for(my $i = 0; $i < $period; ++$i) {
 #print "Total Interest Paid: $total_interest\n";
 #print "Balance: $balance_old $balance\n";
 
-$amortization->print_schedule();
+#$amortization->print_schedule();
